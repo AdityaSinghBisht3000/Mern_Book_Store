d8912dcce0d258899a3570acfa6d803fd01853ca
AdityaSinghBisht <adityasinghbisht1000001@gmail.com>
2024-06-26 19:51:04 +0530

    login updated

--- mern_client/src/components/Login.jsx
+++ mern_client/src/components/Login.jsx
@@ -10,7 +10,7 @@ const Login = () => {
   const navigate = useNavigate();
 
   const from = location.state?.from?.pathname || "/";
-
+            
   const handleLogin = (event) => {
     event.preventDefault();
     const form = event.target;
